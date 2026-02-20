298c1f7580baadd0904dc9982a634228219458ea
melanieob1012 <melanieob1012@gmail.com>
2026-02-20 12:39:13 -0800

    Update lab_1a.py

--- labs/lab_1/lab_1a.py
+++ labs/lab_1/lab_1a.py
@@ -4,12 +4,13 @@ lab_1a.py
 The first lab in the BWSI CSS course. To complete this lab, fill out the variable on line 10
 with your name. Then, save the code, add it to the staging area, and commit it to the Git tree.
 """
+This is to simulate a change made on a robot: robot_speed = 5 # m/s
 
 def main():
     print("Hello World!")
 
     name = "Melanie Ontiveros" # TODO: Insert your name between the double quotes
-This is to simulate a change made on a robot: robot_speed = 5 # m/s
+
     print(f"{name}, Welcome to the CSS course!")
 print("Hi Everyone! My Name Is Melanie , and I am a junior in California. I am really excited for thi course and for the many things I will learn! A cool, fun fact about me is that I do competitive figure skating!")
 if __name__ == "__main__":
